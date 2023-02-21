8921eb28a8e7b46d8053a0d362d7bf064f5959ad
mmunoz <manuel.munoz@westernasset.com>
2023-02-20 18:18:17 -0600

    Initial state

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -13,12 +13,9 @@ import { Component, Input } from '@angular/core';
   ],
 })
 export class CardComponent {
-  @Input() type: string = '';
   @Input() isOpen: boolean = true;
   @Input() img: string = '';
   @Input() title: string = '';
-  @Input() text: string = '';
-  @Input() btnText: string = '';
 
   constructor() {}
 
